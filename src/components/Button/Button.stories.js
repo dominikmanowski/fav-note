31abc02a0002768bfa03045227e780f6ba00be1b
Dominik Manowski <manowski@protonmail.com>
2020-02-16 19:29:47 +0100

    Add Storybook

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Button from './Button';
+
+export default { title: 'Button' };
+
+export const Primary = () => <Button>Primary</Button>;
+export const Secondary = () => <Button secondary>Secondary</Button>;
