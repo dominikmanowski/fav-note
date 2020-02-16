770ddf6db6f06f47abee7f2f69a0ff62cb32f543
Dominik Manowski <manowski@protonmail.com>
2020-02-16 10:44:30 +0100

    Add GlobalStyle component for global css styles

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -7,8 +7,7 @@ const Button = styled.button`
   height: 47px;
   border: none;
   border-radius: 50px;
-  font-family: 'Montserrat';
-  font-weight: 500;
+  font-weight: 300;
   font-size: 16px;
   text-transform: uppercase;
 
--- src/views/Root/Root.jsx
+++ src/views/Root/Root.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Button from 'components/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
 const Root = () => (
   <>
+    <GlobalStyle />
     <h1>Hello</h1>
     <Button width="500px">Close / Save</Button>
     <Button>Close / Save</Button>
