405ae2767a42b4d3bb686e3d15e5ed1217c543cf
Dominik Manowski <manowski@protonmail.com>
2020-02-24 20:30:38 +0100

    Add avatar and icon in card

--- src/components/molecules/Card/Card.jsx
+++ src/components/molecules/Card/Card.jsx
@@ -4,6 +4,7 @@ import styled, { css } from 'styled-components';
 import Paragraph from 'components/atoms/Paragraph';
 import Heading from 'components/atoms/Heading';
 import Button from 'components/atoms/Button';
+import LinkIcon from 'assets/icons/link.svg';
 
 const StyledWrapper = styled.div`
   min-height: 380px;
@@ -16,6 +17,7 @@ const StyledWrapper = styled.div`
 `;
 
 const InnerWrapper = styled.div`
+  position: relative;
   padding: 17px 30px;
   background-color: ${({ activeColor, theme }) => theme[activeColor]};
 
@@ -38,11 +40,40 @@ const StyledHeading = styled(Heading)`
   margin: 10px 0 0;
 `;
 
+const StyledAvatar = styled.img`
+  width: 86px;
+  height: 86px;
+  border: 5px solid ${({ theme }) => theme.twitter};
+  border-radius: 50%;
+  position: absolute;
+  top: 25px;
+  right: 25px;
+`;
+
+const StyledLinkButton = styled.a`
+  display: block;
+  width: 47px;
+  height: 47px;
+  border-radius: 50%;
+  background: white url(${LinkIcon});
+  background-size: 50%;
+  background-position: 50%;
+  background-repeat: no-repeat;
+  position: absolute;
+  transform: translateY(-50%);
+  right: 25px;
+  top: 50%;
+`;
+
 const Card = ({ cardType }) => (
   <StyledWrapper>
     <InnerWrapper activeColor={cardType}>
       <StyledHeading>Hello World</StyledHeading>
       <DateInfo>3 days</DateInfo>
+      {cardType === 'twitter' && (
+        <StyledAvatar src="https://avatars.io/twitter/mowinik" />
+      )}
+      {cardType === 'article' && <StyledLinkButton href="https://google.com" />}
     </InnerWrapper>
     <InnerWrapper flex>
       <Paragraph>
